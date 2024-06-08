b0aba10d641942be9ebb65065ca5ffc0b1708061
Mdafsarx <mdafsar99009@gmail.com>
2024-06-08 15:49:10 +0600

    findpayment api created done

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express=require('express');
-const cors=require('cors')
-const app=express()
-const port=process.env.PORT||1000;
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+const app = express()
+const stripe = require("stripe")(process.env.DB_STRIPE)
+const port = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json())
 
 
 app.use(
-    cors({
-      origin: [
-        "http://localhost:5173",
-      ]
-    })
-  );
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ]
+  })
+);
 
 
 
@@ -35,61 +37,109 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)     
     // data base collection here
 
-    const apartmentCollection=client.db("onebuildDB").collection('apartment');
-    const agreementCollection=client.db("onebuildDB").collection('agreement');
-    const agreementCollectionAdmin=client.db("onebuildDB").collection('agreementAdmin');
-    const userCollection=client.db("onebuildDB").collection('user')
-    const announcementCollection=client.db("onebuildDB").collection('announcement')
+    const apartmentCollection = client.db("onebuildDB").collection('apartment');
+    const agreementCollection = client.db("onebuildDB").collection('agreement');
+    const agreementCollectionAdmin = client.db("onebuildDB").collection('agreementAdmin');
+    const userCollection = client.db("onebuildDB").collection('user')
+    const announcementCollection = client.db("onebuildDB").collection('announcement')
+    const paymentCollection = client.db("onebuildDB").collection('payment')
+
+
+    // payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ['card'],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+
+    })
+    app.post('/payment', async (req, res) => {
+      const result = await paymentCollection.insertOne(req.body);
+      res.send(result);
+    })
+    app.get('/payment/:email', async (req, res) => {
+      const result = await paymentCollection.find({ email: req.params.email }).toArray();
+      res.send(result)
+    })
+    app.get('/findPayment',async(req,res)=>{
+      let query = {};
+      if (req.query.search) {
+        query = {
+          email:req.query.email,
+          month: { $regex: req.query.search, $options: "i" }
+        }
+      }
+      const result=await paymentCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
 
 
     // apartment
-    app.get('/apartment',async(req,res)=>{
-        const result=await apartmentCollection.find().toArray();
-        res.send(result)
+    app.get('/apartment', async (req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result)
     })
 
+
+
     // agreement
-    app.post('/agreement',async(req,res)=>{
-      const find=await agreementCollection.findOne({email:req.body.email})
-      if(find)return res.send({message:'already added'})
-      const result=await agreementCollection.insertOne(req.body)
+    app.post('/agreement', async (req, res) => {
+      const find = await agreementCollection.findOne({ email: req.body.email })
+      if (find) return res.send({ message: 'already added' })
+      const result = await agreementCollection.insertOne(req.body)
       res.send(result)
     })
-    app.post('/agreementAdmin',async(req,res)=>{
-      const find=await agreementCollection.findOne({email:req.body.email})
-      const find2=await agreementCollectionAdmin.findOne({email:req.body.email})
-      if(find&&find2)return res.send({message:'already added'})
-      const result=await agreementCollectionAdmin.insertOne(req.body)
+
+    app.delete('/agreement/:email', async (req, res) => {
+      const result = await agreementCollection.deleteOne({ email: req.params.email });
+      res.send(result);
+    })
+
+
+    app.post('/agreementAdmin', async (req, res) => {
+      const find = await agreementCollection.findOne({ email: req.body.email })
+      const find2 = await agreementCollectionAdmin.findOne({ email: req.body.email })
+      if (find && find2) return res.send({ message: 'already added' })
+      const result = await agreementCollectionAdmin.insertOne(req.body)
       res.send(result)
     })
-    app.delete('/agreementAdmin',async(req,res)=>{
-        const result=await agreementCollectionAdmin.deleteOne({_id:new ObjectId(req.query.id)});
-        res.send(result)
+    app.delete('/agreementAdmin', async (req, res) => {
+      const result = await agreementCollectionAdmin.deleteOne({ _id: new ObjectId(req.query.id) });
+      res.send(result)
     })
-    app.put('/agreement',async(req,res)=>{
-      const filter={email:req.query.email};
-      const updateDoc={
-        $set:{
-          status:req.body.status,
-          year1:req.body.year1,
-          mount1:req.body.mount1,
-          date1:req.body.date1,
+    app.put('/agreement', async (req, res) => {
+      const filter = { email: req.query.email };
+      const updateDoc = {
+        $set: {
+          status: req.body.status,
+          year1: req.body.year1,
+          mount1: req.body.mount1,
+          date1: req.body.date1,
         }
       }
-      const result=await agreementCollection.updateOne(filter,updateDoc);
+      const result = await agreementCollection.updateOne(filter, updateDoc);
       res.send(result)
     })
-    app.get('/agreement',async(req,res)=>{
-      const result=await agreementCollection.find().toArray();
+    app.get('/agreement', async (req, res) => {
+      const result = await agreementCollection.find().toArray();
       res.send(result)
     })
-    app.get('/agreementAdmin',async(req,res)=>{
-      const result=await agreementCollectionAdmin.find().toArray();
+    app.get('/agreementAdmin', async (req, res) => {
+      const result = await agreementCollectionAdmin.find().toArray();
       res.send(result)
     })
-    app.get('/agreement/:email',async(req,res)=>{
+    app.get('/agreement/:email', async (req, res) => {
       console.log(req.params.email)
-      const result=await agreementCollection.findOne({email:req.params.email})
+      const result = await agreementCollection.findOne({ email: req.params.email })
       res.send(result);
     })
 
@@ -97,36 +147,36 @@ async function run() {
 
 
     // user 
-    app.post('/user',async(req,res)=>{
-       const user=req.body;
-       const find=await userCollection.findOne({email:user.email})
-       if(find)return res.send({message:'this user was already added'})
-       const result=await userCollection.insertOne(user) 
-       res.send(result)
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      const find = await userCollection.findOne({ email: user.email })
+      if (find) return res.send({ message: 'this user was already added' })
+      const result = await userCollection.insertOne(user)
+      res.send(result)
     })
-    app.put('/user',async(req,res)=>{
-       const updateDoc={
-        $set:{
-          userRole:req.body.Role
+    app.put('/user', async (req, res) => {
+      const updateDoc = {
+        $set: {
+          userRole: req.body.Role
         }
-       }
-       const result=await userCollection.updateOne({email:req.query.email},updateDoc);
-       res.send(result)
+      }
+      const result = await userCollection.updateOne({ email: req.query.email }, updateDoc);
+      res.send(result)
     })
-    app.get('/users',async(req,res)=>{
-      const result=await userCollection.find().toArray();
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result)
     })
 
 
 
     // announcement
-    app.post('/announcement',async(req,res)=>{
-       const result=await announcementCollection.insertOne(req.body);
-       res.send(result)
+    app.post('/announcement', async (req, res) => {
+      const result = await announcementCollection.insertOne(req.body);
+      res.send(result)
     })
-    app.get('/announcement',async(req,res)=>{
-      const result=await announcementCollection.find().toArray();
+    app.get('/announcement', async (req, res) => {
+      const result = await announcementCollection.find().toArray();
       res.send(result)
     })
 
@@ -167,9 +217,9 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('ph-12-as-server is running')
+app.get('/', (req, res) => {
+  res.send('ph-12-as-server is running')
 })
-app.listen(port,()=>{
-    console.log('server running properly')
+app.listen(port, () => {
+  console.log('server running properly')
 })
